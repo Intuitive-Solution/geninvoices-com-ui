751a8a5ad424573e00da2704d744c7573289c057
Tahir-Imran <timran@stratacache.com>
2024-12-21 17:23:29 +0530

    Removed side bar links

--- src/components/layouts/Default.tsx
+++ src/components/layouts/Default.tsx
@@ -229,40 +229,40 @@ export function Default(props: Props) {
         visible: hasPermission('create_credit'),
       },
     },
-    {
-      name: t('projects'),
-      href: '/projects',
-      icon: Briefcase,
-      current: location.pathname.startsWith('/projects'),
-      visible:
-        enabled(ModuleBitmask.Projects) &&
-        (hasPermission('view_project') ||
-          hasPermission('create_project') ||
-          hasPermission('edit_project')),
-      rightButton: {
-        icon: PlusCircle,
-        to: '/projects/create',
-        label: t('new_project'),
-        visible: hasPermission('create_project'),
-      },
-    },
-    {
-      name: t('tasks'),
-      href: '/tasks',
-      icon: Clock,
-      current: location.pathname.startsWith('/tasks'),
-      visible:
-        enabled(ModuleBitmask.Tasks) &&
-        (hasPermission('view_task') ||
-          hasPermission('edit_task') ||
-          hasPermission('create_task')),
-      rightButton: {
-        icon: PlusCircle,
-        to: '/tasks/create',
-        label: t('new_task'),
-        visible: hasPermission('create_task'),
-      },
-    },
+    // {
+    //   name: t('projects'),
+    //   href: '/projects',
+    //   icon: Briefcase,
+    //   current: location.pathname.startsWith('/projects'),
+    //   visible:
+    //     enabled(ModuleBitmask.Projects) &&
+    //     (hasPermission('view_project') ||
+    //       hasPermission('create_project') ||
+    //       hasPermission('edit_project')),
+    //   rightButton: {
+    //     icon: PlusCircle,
+    //     to: '/projects/create',
+    //     label: t('new_project'),
+    //     visible: hasPermission('create_project'),
+    //   },
+    // },
+    // {
+    //   name: t('tasks'),
+    //   href: '/tasks',
+    //   icon: Clock,
+    //   current: location.pathname.startsWith('/tasks'),
+    //   visible:
+    //     enabled(ModuleBitmask.Tasks) &&
+    //     (hasPermission('view_task') ||
+    //       hasPermission('edit_task') ||
+    //       hasPermission('create_task')),
+    //   rightButton: {
+    //     icon: PlusCircle,
+    //     to: '/tasks/create',
+    //     label: t('new_task'),
+    //     visible: hasPermission('create_task'),
+    //   },
+    // },
     {
       name: t('vendors'),
       href: '/vendors',
